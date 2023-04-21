05929b25c9074ba625f53943374ae3174867cda0
Collins Okafor <Collinszurum@gmail.com>
2023-04-21 19:47:59 +0100

    Bootstrap cdn added

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,7 +1,15 @@
+using FrontEnd.ConstantHolder;
+using FrontEnd.Services;
+using FrontEnd.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<IProductService, ProductService>();
+SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
